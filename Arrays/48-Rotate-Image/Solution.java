4b8e46c6ba49325228b589a9985356c5a888e39d
sahasradaragani1-rgb <sahasradaragani1@gmail.com>
2026-06-14 12:21:10 +0530

    Create Solution.java

---
+++ Arrays/48-Rotate-Image/Solution.java
@@ -0,0 +1,71 @@
+/*
+LeetCode 48: Rotate Image
+
+Approach:
+1. Transpose the matrix.
+2. Reverse each row.
+
+This rotates the matrix by 90 degrees clockwise in-place.
+
+Time Complexity: O(n²)
+Space Complexity: O(1)
+*/
+
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+
+        Scanner in = new Scanner(System.in);
+
+        System.out.print("Enter size of matrix: ");
+        int n = in.nextInt();
+
+        int[][] matrix = new int[n][n];
+
+        System.out.println("Enter matrix elements:");
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                matrix[i][j] = in.nextInt();
+            }
+        }
+
+        // Transpose
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
+
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[j][i];
+                matrix[j][i] = temp;
+            }
+        }
+
+        // Reverse each row
+        for (int i = 0; i < n; i++) {
+
+            int left = 0;
+            int right = n - 1;
+
+            while (left < right) {
+
+                int temp = matrix[i][left];
+                matrix[i][left] = matrix[i][right];
+                matrix[i][right] = temp;
+
+                left++;
+                right--;
+            }
+        }
+
+        System.out.println("Rotated Matrix:");
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                System.out.print(matrix[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
