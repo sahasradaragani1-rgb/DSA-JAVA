2d0aa627d54b755aa39cf5969f8857e5a7b18a75
sahasradaragani1-rgb <sahasradaragani1@gmail.com>
2026-06-15 20:48:43 +0530

    Create Solution.java

---
+++ Arrays/169-Majority-Element/Solution.java
@@ -0,0 +1,50 @@
+/*
+LeetCode 169: Majority Element
+
+Approach:
+Moore's Voting Algorithm
+
+A majority element appears more than n/2 times.
+
+Time Complexity: O(n)
+Space Complexity: O(1)
+*/
+
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+
+        Scanner in = new Scanner(System.in);
+
+        System.out.print("Enter size of array: ");
+        int n = in.nextInt();
+
+        int[] nums = new int[n];
+
+        System.out.println("Enter array elements:");
+
+        for (int i = 0; i < n; i++) {
+            nums[i] = in.nextInt();
+        }
+
+        int candidate = 0;
+        int count = 0;
+
+        for (int num : nums) {
+
+            if (count == 0) {
+                candidate = num;
+            }
+
+            if (num == candidate) {
+                count++;
+            } else {
+                count--;
+            }
+        }
+
+        System.out.println("Majority Element = " + candidate);
+    }
+}
