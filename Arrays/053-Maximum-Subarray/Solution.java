6a53b90adbc389244b2b246f03f005810c0863ce
sahasradaragani1-rgb <sahasradaragani1@gmail.com>
2026-06-15 20:52:14 +0530

    Create Solution.java

---
+++ Arrays/053-Maximum-Subarray/Solution.java
@@ -0,0 +1,49 @@
+/*
+LeetCode 53: Maximum Subarray
+
+Approach:
+Kadane's Algorithm
+
+If the current sum becomes negative,
+discard it and start a new subarray.
+
+Time Complexity: O(n)
+Space Complexity: O(1)
+*/
+
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+
+        Scanner in = new Scanner(System.in);
+
+        System.out.print("Enter size of array: ");
+        int n = in.nextInt();
+
+        int[] nums = new int[n];
+
+        System.out.println("Enter array elements:");
+
+        for (int i = 0; i < n; i++) {
+            nums[i] = in.nextInt();
+        }
+
+        int sum = 0;
+        int maxSum = Integer.MIN_VALUE;
+
+        for (int i = 0; i < n; i++) {
+
+            sum += nums[i];
+
+            maxSum = Math.max(maxSum, sum);
+
+            if (sum < 0) {
+                sum = 0;
+            }
+        }
+
+        System.out.println("Maximum Subarray Sum = " + maxSum);
+    }
+}
