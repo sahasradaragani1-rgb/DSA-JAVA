bfb12970566df46332e611d9cfbaadfd0291220b
sahasradaragani1-rgb <sahasradaragani1@gmail.com>
2026-06-17 20:13:55 +0530

    Create Solution.java

---
+++ Arrays/128-Longest-Consecutive-Sequence/Solution.java
@@ -0,0 +1,62 @@
+/*
+LeetCode 128: Longest Consecutive Sequence
+
+Approach:
+Store all elements in a HashSet.
+
+A number is the start of a sequence if:
+(num - 1) is not present.
+
+Then count consecutive numbers.
+
+Time Complexity: O(n)
+Space Complexity: O(n)
+*/
+
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+
+        Scanner in = new Scanner(System.in);
+
+        System.out.print("Enter size of array: ");
+        int n = in.nextInt();
+
+        int[] nums = new int[n];
+
+        System.out.println("Enter array elements:");
+
+        for (int i = 0; i < n; i++) {
+            nums[i] = in.nextInt();
+        }
+
+        HashSet<Integer> set = new HashSet<>();
+
+        for (int num : nums) {
+            set.add(num);
+        }
+
+        int longest = 0;
+
+        for (int num : set) {
+
+            // Start of sequence
+            if (!set.contains(num - 1)) {
+
+                int currentNum = num;
+                int count = 1;
+
+                while (set.contains(currentNum + 1)) {
+                    currentNum++;
+                    count++;
+                }
+
+                longest = Math.max(longest, count);
+            }
+        }
+
+        System.out.println("Longest Consecutive Sequence Length = " + longest);
+    }
+}
