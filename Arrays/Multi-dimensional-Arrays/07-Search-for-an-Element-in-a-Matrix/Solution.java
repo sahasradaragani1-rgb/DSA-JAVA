571a88cf66b1dae0cd2d5b5d7be08576ec33559b
sahasradaragani1-rgb <sahasradaragani1@gmail.com>
2026-08-14 13:34:50 +0530

    Create Solution.java

---
+++ Arrays/Multi-dimensional-Arrays/07-Search-for-an-Element-in-a-Matrix/Solution.java
@@ -0,0 +1,65 @@
+import java.util.Scanner;
+
+public class Solution {
+
+public static void main(String[] args) {
+
+    Scanner sc = new Scanner(System.in);
+
+    // Read number of rows
+    int rows = sc.nextInt();
+
+    // Create jagged 2D array
+    int[][] matrix = new int[rows][];
+
+    // Read each row
+    for (int i = 0; i < rows; i++) {
+
+        // Read number of elements in current row
+        int cols = sc.nextInt();
+
+        matrix[i] = new int[cols];
+
+        // Read elements
+        for (int j = 0; j < cols; j++) {
+            matrix[i][j] = sc.nextInt();
+        }
+    }
+
+    // Read target element
+    int target = sc.nextInt();
+
+    boolean found = false;
+
+    // Search for the target
+    for (int i = 0; i < matrix.length; i++) {
+
+        for (int j = 0; j < matrix[i].length; j++) {
+
+            if (matrix[i][j] == target) {
+
+                System.out.println(
+                    "Element found at row " + i + ", column " + j
+                );
+
+                found = true;
+                break;
+            }
+        }
+
+        // Stop searching after finding the first occurrence
+        if (found) {
+            break;
+        }
+    }
+
+    // Target not found
+    if (!found) {
+        System.out.println("Element not found");
+    }
+
+    sc.close();
+}
+
+
+}
